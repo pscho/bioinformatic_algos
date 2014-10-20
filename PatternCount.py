badaeef9bf4a661ed35c9e8a4e77967861453b3a
pscho <highencast@gmail.com>
2014-10-20 02:16:28 -0400

    finished patterncount

--- PatternCount.py
+++ PatternCount.py
@@ -1,19 +1,38 @@
 # Bioinformatic Algorithms #
+# PatternCount algorithm (naive?) #
 
-def readToList(filename):
-    with open(filename, "r") as handle:
-        retVal = handle.read().splitlines()
+def main():
+    # input
+    filename = "dataset_2_6.txt" ## Change this ##
 
-    return retVal
-
-def patternCount():
+    handle = open(filename, "r")
+    data = handle.read().splitlines()
+    handle.close()
     
-    filename = "test.txt" ## Change this ##
+    matchCount = patternCount(data[0], data[1])
 
-    text = readToList(filename)
+    # write results
+    handle = open("PatternCount.txt", "w")
+    handle.write("Input\n" +
+                 data[0] + "\n" + 
+                 data[1] + "\n" + 
+                 "Output" + "\n" + 
+                 str(matchCount) + "\n")
+    handle.close()
 
-    print text
+def patternCount(text, pattern):
+    count = 0
 
+    for i in range(0, (len(text) - len(pattern)) + 1):
+        # get word
+        word = ""
+        for j in range(i, i + len(pattern)):
+            word = word + text[j]
 
+        if pattern == word:
+            count += 1
+
+    return count
+            
 if __name__ == "__main__":
-    patternCount()
+    main()
