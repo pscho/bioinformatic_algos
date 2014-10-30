1d769210b148f2ab4b9c994d79d0c46a888981e6
pscho <highencast@gmail.com>
2014-10-29 23:42:48 -0400

    input data for Skew.py

--- Skew.py
+++ Skew.py
@@ -1,6 +1,11 @@
 
 def skew_main():
-    pass
+    filename = "test_data.txt" ## Change this
+
+    with open("test_data.txt", "r") as handle:
+        data = handle.read()
+
+    # call skew
 
 if __name__ == "__main__":
-    skew_main():
+    skew_main()
